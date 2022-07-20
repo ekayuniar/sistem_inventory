1a658d0c11fc9fda66e8af2bd2a80a5c90ed4c24
eka yuniar <sayayuniar@gmail.com>
2022-07-20 18:28:03 +0700

    update pelanggan

--- function.php
+++ function.php
@@ -1,117 +1,252 @@
 <?php
-session_start();
+require 'ceklogin.php';
+$h1 = mysqli_query($koneksi, "SELECT * FROM pelanggan");
+$h2 = mysqli_num_rows($h1);
 
-$koneksi = mysqli_connect('localhost', 'root', '', 'inventory');
+?>
+<!DOCTYPE html>
+<html lang="en">
 
-if (isset($_POST['login'])) {
-    //initial variable
-    $username = $_POST['username'];
-    $password = $_POST['password'];
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Aplikasi Kasir</title>
+    <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet" />
+    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+</head>
 
-    $check = mysqli_query(
-        $koneksi,
-        "SELECT * FROM user 
-        WHERE username='$username' 
-            AND password='$password'"
-    );
-    $hitung = mysqli_num_rows($check);
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <!-- Navbar Brand-->
+        <a class="navbar-brand ps-3" href="index.html">Aplikasi Kasir</a>
+        <!-- Sidebar Toggle-->
+        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
+                class="fas fa-bars"></i></button>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Menu</div>
+                        <a class="nav-link" href="index.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Order
+                        </a>
+                        <a class="nav-link" href="stock.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Stock Barang
+                        </a>
+                        <a class="nav-link" href="masuk.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Barang Masuk
+                        </a>
+                        <a class="nav-link" href="pelanggan.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Kelola Pelanggan
+                        </a>
+                        <a class="nav-link" href="logout.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-sign-out-alt"></i></div>
+                            Logout
+                        </a>
 
-    if ($hitung > 0) {
-        // jika datanya ada, dan ditemukan 
-        // berhasil login
-        $_SESSION['login'] = true;
-        header('location:index.php');
-    } else {
-        //Datanya g ada
-        // gagal login
-        echo '
-        <script>
-        alert("Username atau Password salah")
-        window.location.href="login.php"
-        </script>';
-    }
-}
+                        <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne"
+                            data-bs-parent="#sidenavAccordion">
+                            <nav class="sb-sidenav-menu-nested nav">
+                                <a class="nav-link" href="layout-static.html">Static Navigation</a>
+                                <a class="nav-link" href="layout-sidenav-light.html">Light Sidenav</a>
+                            </nav>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid px-4">
+                    <h1 class="mt-4">Kelola Pelanggan</h1>
+                    <ol class="breadcrumb mb-4">
+                    </ol>
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="card-body">Jumlah Pelanggan : <?= $h2;  ?></div>
+                            </div>
+                            <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                                data-bs-target="#myModal">
+                                Tambah Pelanggan
+                            </button>
+                            <div class="container mt-3">
 
-if (isset($_POST['tambahpelanggan'])) {
-    // initial variable
-    $nama_pelanggan = $_POST['nama_pelanggan'];
-    $no_telp = $_POST['no_telp'];
-    $alamat = $_POST['alamat'];
+                            </div>
+                        </div>
 
-    $tambahpelanggan = mysqli_query(
-        $koneksi,
-        "INSERT INTO pelanggan (nama_pelanggan, no_telp, alamat) 
-   VALUES ('$nama_pelanggan','$no_telp','$alamat')"
-    );
+                    </div>
 
-    if ($tambahpelanggan) {
-        // kalau sukses
-        header('location:pelanggan.php');
-    } else {
-        echo '<script>
-    alert("Gagal Tambag Pelanggan")
-    window.location.href="pelanggan.php"
-    </script>';
-    }
-}
+                    <div class="card mb-4">
+                        <div class="card-header">
+                            <i class="fas fa-table me-1"></i>
+                            Data Pelanggan
+                        </div>
+                        <div class="card-body">
+                            <table id="datatablesSimple">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Nama Pelanggan</th>
+                                        <th>No. Telp</th>
+                                        <th>Alamat</th>
+                                        <th>Aksi</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $getpelanggan = mysqli_query($koneksi, "SELECT * FROM pelanggan");
+                                    $i = 1;
 
-if (isset($_POST['hapuspelanggan'])) {
-    $id_pelanggan = $_POST['id_pelanggan'];
+                                    while ($pl = mysqli_fetch_array($getpelanggan)) {
+                                        $id_pelanggan = $pl['id_pelanggan'];
+                                        $nama_pelanggan = $pl['nama_pelanggan'];
+                                        $notelp = $pl['notelp'];
+                                        $alamat = $pl['alamat'];
 
-    $hapuspelanggan = mysqli_query($koneksi, "DELETE FROM pelanggan WHERE id_pelanggan='$id_pelanggan'");
+                                    ?>
+                                    <tr>
+                                        <td><?= $i++;  ?></td>
+                                        <td><?= $nama_pelanggan;  ?></td>
+                                        <td><?= $notelp;  ?></td>
+                                        <td><?= $alamat;  ?></td>
+                                        <td><button type="button" class="btn btn-warning" data-bs-toggle="modal"
+                                                data-bs-target="#edit<?= $id_pelanggan; ?>">
+                                                Edit
+                                            </button>
+                                            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                                                data-bs-target="#delete<?= $id_pelanggan; ?>">
+                                                Delete
+                                            </button>
+                                        </td>
+                                    </tr>
+                                    <!-- Modal Edit -->
+                                    <div class="modal" id="edit<?= $id_pelanggan; ?>">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
 
-    if ($hapuspelanggan) {
-        // kalau sukses
-        header('location:pelanggan.php');
-    } else {
-        echo '<script> 
-        alert("Gagal Hapus Pelanggan")
-        window.location.href="pelanggan.php"
-        </script>';
-    }
-}
+                                                <!-- Modal Header -->
+                                                <div class="modal-header">
+                                                    <h4 class="modal-title">Edit Pelanggan <?= $nama_pelanggan;  ?></h4>
+                                                    <button type="button" class="btn-close"
+                                                        data-bs-dismiss="modal"></button>
+                                                </div>
+                                                <form method="POST">
+                                                    <!-- Modal body -->
+                                                    <div class="modal-body">
+                                                        <input type="text" name="nama_pelanggan"
+                                                            class="form-control mt-3" value="<?= $nama_pelanggan; ?>">
+                                                        <input type="text" name="notelp" class="form-control mt-3"
+                                                            value="<?= $notelp;  ?>">
+                                                        <input type="text" name="alamat" class="form-control mt-3"
+                                                            value="<?= $alamat;  ?>">
+                                                        <input type="hidden" name="id_pelanggan"
+                                                            class="form-control mt-3" value="<?= $id_pelanggan;  ?>">
+                                                    </div>
 
-if (isset($_POST['editpelanggan'])) {
-    // initial variable
-    $nama_pelanggan = $_POST['nama_pelanggan'];
-    $no_telp = $_POST['no_telp'];
-    $alamat = $_POST['alamat'];
-    $id_pelanggan = $_POST['id_pelanggan'];
+                                                    <!-- Modal footer -->
+                                                    <div class="modal-footer">
+                                                        <button type="submit" class="btn btn-success"
+                                                            name="editpelanggan">Simpan</button>
+                                                        <button type="button" class="btn btn-danger"
+                                                            data-bs-dismiss="modal">Tutup</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
 
-    $editpelanggan = mysqli_query($koneksi, "UPDATE pelanggan SET 
-        nama_pelanggan='$nama_pelanggan', 
-        no_telp='$no_telp', 
-        alamat='$alamat' 
-            WHERE id_pelanggan='$id_pelanggan'");
 
-    if ($editpelanggan) {
-        // kalau sukses
-        header('location:pelanggan.php');
-    } else {
-        echo '<script> 
-            alert("Gagal Edit Pelanggan")
-            window.location.href="pelanggan.php"
-            </script>';
-    }
-}
+                                    <!-- Modal Hapus -->
+                                    <div class="modal" id="delete<?= $id_pelanggan; ?>">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content">
 
-if (isset($_POST['tambahbarang'])) {
-    //deskripsi initial variable
-    $nama_produk = $_POST['nama_produk'];
-    $deskripsi = $_POST['deskripsi'];
-    $harga = $_POST['harga'];
-    $stock = $_POST['stock'];
+                                                <!-- Modal Header -->
+                                                <div class="modal-header">
+                                                    <h4 class="modal-title">Delete Barang <?= $nama_pelanggan;  ?></h4>
+                                                    <button type="button" class="btn-close"
+                                                        data-bs-dismiss="modal"></button>
+                                                </div>
+                                                <form method="POST">
+                                                    <!-- Modal body -->
+                                                    <div class="modal-body">
+                                                        Apakah Anda Yakin akan menghapus Pelanggan ini?
+                                                        <input type="hidden" name="id_pelanggan"
+                                                            class="form-control mt-3" value="<?= $id_pelanggan;  ?>">
+                                                    </div>
 
-    $insert_produk = mysqli_query($koneksi, "INSERT INTO produk (nama_produk, deskripsi, harga, stock) 
-    VALUES ('$nama_produk', '$deskripsi', '$harga', '$stock')");
+                                                    <!-- Modal footer -->
+                                                    <div class="modal-footer">
+                                                        <button type="submit" class="btn btn-success"
+                                                            name="hapuspelanggan">Hapus</button>
+                                                        <button type="button" class="btn btn-danger"
+                                                            data-bs-dismiss="modal">Tutup</button>
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <?php }; ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid px-4">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Eka Yuniar 2022</div>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous">
+    </script>
+    <script src="js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="assets/demo/chart-area-demo.js"></script>
+    <script src="assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@latest" crossorigin="anonymous"></script>
+    <script src="js/datatables-simple-demo.js"></script>
+</body>
+<div class="modal" id="myModal">
+    <div class="modal-dialog">
+        <div class="modal-content">
 
-    if ($insert_produk) {
-        header('location:stock.php');
-    } else {
-        echo '
-        <script>
-        alert("Gagal Tambah Produk")
-        window.location.href="stock.php"
-        </script>';
-    }
-}
+            <!-- Modal Header -->
+            <div class="modal-header">
+                <h4 class="modal-title">Data Tambah Pelanggan</h4>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+            </div>
+            <form method="POST">
+                <!-- Modal body -->
+                <div class="modal-body">
+                    <input type="text" name="nama_pelanggan" class="form-control mt-3" placeholder="nama pelanggan">
+                    <input type="text" name="notelp" class="form-control mt-3" placeholder="no telp">
+                    <input type="text" name="alamat" class="form-control mt-3" placeholder="alamat">
+                </div>
+
+                <!-- Modal footer -->
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-success" name="tambahpelanggan">Simpan</button>
+                    <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Tutup</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+</html>
